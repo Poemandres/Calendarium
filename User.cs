eeef3b3e212b1213b93234fd98d4f4ee3606d137
aludesk <aludesk@ARI_LAB0212NT>
2022-06-01 16:55:20 -0400

    Primer cambio, 1 clase añadida

---
+++ User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class User
+{
+
+	public static void Main()
+	{
+		Console.WriteLine("Clase plantilla, jaja");
+
+		private String username; 
+		private String password;
+		private int idUser;
+		private String email;
+
+	}
+}
