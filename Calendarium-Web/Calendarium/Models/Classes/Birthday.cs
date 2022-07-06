914b12d060f476193f6b99a2ea1b1439035dd28c
Poemandres <104645823+Poemandres@users.noreply.github.com>
2022-07-06 17:16:38 -0400

    Trabajadas vistas y eso

---
+++ Calendarium-Web/Calendarium/Models/Classes/Birthday.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Birthday
+{
+
+		private int personID {get; set;} 
+		private String? personNAME {get; set;} 
+		private DateTime personBIRTHDATE {get; set;}
+		private int personBIRTHYEAR { get; set; }
+
+
+	public Birthday(int personID, String personNAME, DateTime personBIRTHDATE, int personBIRTHYEAR) { 
+		this.personID = personID;
+		this.personNAME = personNAME;
+		this.personBIRTHDATE = personBIRTHDATE;
+		this.personBIRTHYEAR = personBIRTHYEAR;
+	}
+
+}
