4d3f41213ae3d657e116a8e9680508ae71ced1f7
Poemandres <104645823+Poemandres@users.noreply.github.com>
2022-07-13 16:45:24 -0400

    Algo

---
+++ Calendarium-Web/Calendarium/Controllers/MyCalendars.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Calendarium.Controllers
+{
+    public class MyCalendars : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
