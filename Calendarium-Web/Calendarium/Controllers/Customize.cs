1d762c0fb7b76922cca525ba021fec799b54564c
Poemandres <Poemandres@users.noreply.github.com>
2022-07-18 23:57:23 -0400

    Añadido un Full Calendar a la vista de Personalizar

---
+++ Calendarium-Web/Calendarium/Controllers/Customize.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Calendarium.Controllers
+{
+    public class Customize : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
